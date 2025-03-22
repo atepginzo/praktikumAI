0ce0d79539deb218109c4e55e77ad01af6d47d1b
atepginzo <atepginzo037@gmail.com>
2025-03-22 07:00:20 +0700

    Added Procfile and Gunicorn setup

--- venv/app.py
+++ venv/app.py
@@ -15,6 +15,6 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))  
     app.run(debug=False, host='0.0.0.0', port=port)
     
